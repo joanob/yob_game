f69c1081e2cb21ec690b54c3073eda232851afa1
joanob <54626555+joanob@users.noreply.github.com>
2023-06-06 11:24:06 +0200

    add footer navbar

--- front_angular/src/app/components/footer-navbar/footer-navbar.component.ts
+++ front_angular/src/app/components/footer-navbar/footer-navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/auth/user.service';
 
 @Component({
   selector: 'app-footer-navbar',
   templateUrl: './footer-navbar.component.html',
-  styleUrls: ['./footer-navbar.component.scss']
+  styleUrls: ['./footer-navbar.component.scss'],
 })
 export class FooterNavbarComponent {
-
+  constructor(public authService: UserService) {}
 }
